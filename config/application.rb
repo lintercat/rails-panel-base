bf10443615eac64e894955f2d10c7069a97274a0
Daniel Rejon <rejonpardenilla@gmail.com>
2019-02-12 12:57:37 -0600

    Add i18n spanish

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Panel
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = 'es'
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Mexico City'
   end
 end
